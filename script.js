4f39d387f8094f5ad38e1fabba310216c3c848f0
jacobm2000 <maurerja2000@gmail.com>
2022-09-12 13:41:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var time=document.getElementById("time");
-
-
+var year
+var now
+var target
+var days
+var hours
+var minutes
+var seconds
 
 
 updateTime()
@@ -11,10 +16,10 @@ function updateTime(){
      target=new Date("Jan 1,"+year + " 00:00:00").getTime()
     distance=target-now
 
-     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+     seconds = Math.floor((distance % (1000 * 60)) / 1000);
      document.getElementById("days").textContent=days+" "
      document.getElementById("hours").textContent=hours+" "
      document.getElementById("min").textContent=minutes+ " "
